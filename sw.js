ccf85bfde6ebe826396b59ba2cd00bf307b9eddd
akghnekar2020@gmail.com <akghnekar2020@gmail.com>
2022-04-07 21:11:55 +0530

    commit 20

--- sw.js
+++ sw.js
@@ -28,7 +28,7 @@ self.addEventListener('install' , (event)=>{
             'https://akhil24-abd.github.io/burger-website/images/product-4.png',
             'https://akhil24-abd.github.io/burger-website/images/product-5.png',
             'https://akhil24-abd.github.io/burger-website/images/product-6.png',
-            'https://akhil24-abd.github.io/burger-website/images/title-img.png',
+            'https://akhil24-abd.github.io/burger-website/images/title-img.png'
         ]).catch((error)=>{
             console.log(error)
         })
